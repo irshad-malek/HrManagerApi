2b843e522c68bae26cf019d6aefc40d38ce74a19
irshad-malek <irshad.malek@prakashinfotech.com>
2023-04-13 16:25:57 +0530

    assignee changes

---
+++ HRmanagement/Controllers/AssigneeController.cs
@@ -0,0 +1,61 @@
+﻿using Azure.Core;
+using Hrmanagement.DataModel.ViewModel;
+using Hrmanagement.Repository.Entities;
+using Hrmanagement.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRmanagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssigneeController : ControllerBase
+    {
+        private readonly IAssignee assignee;
+
+        public AssigneeController(IAssignee assignee)
+        {
+            this.assignee = assignee;
+        }
+        
+        [HttpPost]
+        [Route("addAssignee/{eId}/{EmpId}")]
+        public async Task<int> addAssignee(int eId, int EmpId)
+        {
+            int data=await JuniourAssign(eId);
+            int data1 = await SeniourAssign(EmpId);
+            return this.assignee.save(data, data1);
+        }
+        [HttpPost]
+        [Route("addjAssignee")]
+        public async Task<int> JuniourAssign(int eId)
+        {
+            return this.assignee.jAssignee(eId);
+        }
+        [HttpPost]
+        [Route("addsAssignee")]
+        public async Task<int> SeniourAssign(int EmpId)
+        {
+            return this.assignee.sAssignee(EmpId);
+        }
+        [HttpGet]
+        [Route("juniourAssign")]
+        public List<EmployeeVm> GetJunioursAssign()
+        {
+            return this.assignee.GetJuniourAssigns();
+        }
+        [HttpGet]
+        [Route("seniourAssign")]
+        public List<EmployeeVm> GetSeniorAssign()
+        {
+            return this.assignee.GetSeniorAssigns();
+        }
+        [HttpGet]
+        [Route("AssignList")]
+
+        public List<AssigneeVm> AssignList()
+        {
+            return this.assignee.GetAssignees();
+        }
+    }
+}
---
+++ Hrmanagement.Repository/Interface/IAssignee.cs
@@ -0,0 +1,26 @@
+﻿using Hrmanagement.DataModel.ViewModel;
+using Hrmanagement.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrmanagement.Repository.Interface
+{
+    public interface IAssignee
+    {
+        int save(int jId, int sId);
+
+        int jAssignee(int empId);
+
+        int sAssignee(int EmpId);
+
+        List<EmployeeVm> GetJuniourAssigns();
+
+        List<EmployeeVm> GetSeniorAssigns();
+
+        List<AssigneeVm> GetAssignees();
+
+    }
+}
