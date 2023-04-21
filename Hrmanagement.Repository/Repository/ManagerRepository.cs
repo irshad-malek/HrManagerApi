5ab0647b33688e4a42d701d5bb332db6c489dd50
irshad-malek <irshad.malek@prakashinfotech.com>
2023-04-21 16:49:22 +0530

    manager push

---
+++ Hrmanagement.Repository/Repository/ManagerRepository.cs
@@ -0,0 +1,26 @@
+﻿using Hrmanagement.DataModel.ViewModel;
+using Hrmanagement.Repository.Entities;
+using Hrmanagement.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrmanagement.Repository.Repository
+{
+    public class ManagerRepository : IManager
+    {
+        public async Task<int> SaveManager(ManagerVm managerVm)
+        {
+            Manager manager = new Manager();
+            manager.EmployeeRoleId = managerVm.EmployeeRoleId;
+            manager.EmpIdMgr = managerVm.EmpIdMgr;
+            manager.DeptId=managerVm.DeptId;
+            manager.EffectiveFromDate = managerVm.EffectiveFromDate;
+            manager.EffectiveFromDate=managerVm.EffectiveFromDate;
+            manager.EmpId = managerVm.EmpId;
+            return manager.ManagerId;
+        }
+    }
+}
